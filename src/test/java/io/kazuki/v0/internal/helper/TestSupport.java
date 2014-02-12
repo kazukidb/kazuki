5ee0d619c17a3ea31f14fa115de769626d0fabdf
Tamas Cservenak <tamas@cservenak.net>
2014-02-12 13:42:20 +0100

    Be more chatty about progress.

--- src/test/java/io/kazuki/v0/internal/helper/TestSupport.java
+++ src/test/java/io/kazuki/v0/internal/helper/TestSupport.java
@@ -3,7 +3,9 @@ package io.kazuki.v0.internal.helper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 
 public abstract class TestSupport
@@ -23,7 +25,27 @@ public abstract class TestSupport
   }
 
   @BeforeClass
-  public void reportProgress() {
+  public void announceClass() {
     System.out.println("Running test " + getClass().getName());
+  }
+
+  @AfterClass
+  public void denounceClass() {
+    System.out.println();
+  }
+
+  @AfterMethod
+  public void reportMethod(final ITestResult result) {
+    final StringBuilder sb = new StringBuilder("[");
+    sb.append(result.getMethod().getMethodName());
+    sb.append(" ");
+    if (result.isSuccess()) {
+      sb.append(" Ok]");
+    }
+    else {
+      sb.append(" Failed]");
+    }
+    sb.append(" ");
+    System.out.print(sb);
   }
 }
