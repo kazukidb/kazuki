4078d6a4353601842d91b559b7231ee28eeee2bd
Sunny Gleason <sunny.gleason@gmail.com>
2014-05-08 08:21:26 -0400

    add CharOneTransform

---
+++ kazuki-impl/src/main/java/io/kazuki/v0/internal/v2schema/types/CharOneTransform.java
@@ -0,0 +1,43 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package io.kazuki.v0.internal.v2schema.types;
+
+import io.kazuki.v0.store.schema.model.Transform;
+import io.kazuki.v0.store.schema.model.TransformException;
+
+
+
+/**
+ * Validates / transforms Boolean values.
+ */
+public class CharOneTransform implements Transform<Object, String> {
+  @Override
+  public String pack(Object instance) throws TransformException {
+    if (instance == null || instance.toString().length() == 0) {
+      throw new TransformException("must not be null");
+    }
+
+    return instance.toString().substring(0, 1);
+  }
+
+  @Override
+  public Object unpack(String instance) throws TransformException {
+    if (instance == null || instance.length() == 0) {
+      throw new TransformException("must not be null");
+    }
+
+    return instance.toString().substring(0, 1);
+  }
+}
--- kazuki-impl/src/main/java/io/kazuki/v0/internal/v2schema/types/TypeTransforms.java
+++ kazuki-impl/src/main/java/io/kazuki/v0/internal/v2schema/types/TypeTransforms.java
@@ -1,19 +1,16 @@
 /**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 package io.kazuki.v0.internal.v2schema.types;
 
@@ -24,52 +21,52 @@ import io.kazuki.v0.store.schema.model.Transform;
  * Helper class for mapping attribute types to ValidatorTransformer instances.
  */
 public class TypeTransforms {
-    public static Transform<?, ?> validatorFor(Attribute attribute) {
-        switch (attribute.getType()) {
-        case ANY:
-            return new AnyTransform();
-        case MAP:
-            return new MapTransform();
-        case ARRAY:
-            return new ArrayTransform();
-        case BOOLEAN:
-            return new BooleanTransform();
-        case ENUM:
-            return new EnumTransform(attribute.getValues());
+  public static Transform<?, ?> validatorFor(Attribute attribute) {
+    switch (attribute.getType()) {
+      case ANY:
+        return new AnyTransform();
+      case MAP:
+        return new MapTransform();
+      case ARRAY:
+        return new ArrayTransform();
+      case BOOLEAN:
+        return new BooleanTransform();
+      case CHAR_ONE:
+        return new CharOneTransform();
+      case ENUM:
+        return new EnumTransform(attribute.getValues());
 
-        case U8:
-            return new IntegerTransform("0", Integer.toString(0x000000FF));
-        case U16:
-            return new IntegerTransform("0", Integer.toString(0x0000FFFF));
-        case U32:
-            return new IntegerTransform("0", Long.toString(0x00000000FFFFFFFFL));
-        case U64:
-            return new IntegerTransform("0", Long.toString(Long.MAX_VALUE)
-            /* was "18446744073709551615", but that's unsupported by h2 */);
+      case U8:
+        return new IntegerTransform("0", Integer.toString(0x000000FF));
+      case U16:
+        return new IntegerTransform("0", Integer.toString(0x0000FFFF));
+      case U32:
+        return new IntegerTransform("0", Long.toString(0x00000000FFFFFFFFL));
+      case U64:
+        /* NOTE: "18446744073709551615" is unsupported by h2 */
+        // return new IntegerTransform("0", Long.toString(Long.MAX_VALUE));
+        return new IntegerTransform("0", "18446744073709551615");
 
-        case I8:
-            return new IntegerTransform(Integer.toString(Byte.MIN_VALUE),
-                    Integer.toString(Byte.MAX_VALUE));
-        case I16:
-            return new IntegerTransform(Integer.toString(-32768),
-                    Integer.toString(32767));
-        case I32:
-            return new IntegerTransform(Integer.toString(Integer.MIN_VALUE),
-                    Integer.toString(Integer.MAX_VALUE));
-        case I64:
-            return new IntegerTransform(Long.toString(Long.MIN_VALUE),
-                    Long.toString(Long.MAX_VALUE));
+      case I8:
+        return new IntegerTransform(Integer.toString(Byte.MIN_VALUE),
+            Integer.toString(Byte.MAX_VALUE));
+      case I16:
+        return new IntegerTransform(Integer.toString(-32768), Integer.toString(32767));
+      case I32:
+        return new IntegerTransform(Integer.toString(Integer.MIN_VALUE),
+            Integer.toString(Integer.MAX_VALUE));
+      case I64:
+        return new IntegerTransform(Long.toString(Long.MIN_VALUE), Long.toString(Long.MAX_VALUE));
 
-        case UTC_DATE_SECS:
-            return new UTCDateTransform();
-        case UTF8_SMALLSTRING:
-            return new SmallStringTransform();
-        case UTF8_TEXT:
-            return new TextTransform();
+      case UTC_DATE_SECS:
+        return new UTCDateSecsTransform();
+      case UTF8_SMALLSTRING:
+        return new SmallStringTransform();
+      case UTF8_TEXT:
+        return new TextTransform();
 
-        default:
-            throw new IllegalArgumentException("Unknown type "
-                    + attribute.getType());
-        }
+      default:
+        throw new IllegalArgumentException("Unknown type " + attribute.getType());
     }
+  }
 }
