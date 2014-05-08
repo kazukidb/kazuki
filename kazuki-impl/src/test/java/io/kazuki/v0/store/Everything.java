febf89cf7fa7ad5b8a85a082984554dda1fe1114
Sunny Gleason <sunny.gleason@gmail.com>
2014-05-08 08:24:27 -0400

    add test for all attribute types

---
+++ kazuki-impl/src/test/java/io/kazuki/v0/store/Everything.java
@@ -0,0 +1,97 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package io.kazuki.v0.store;
+
+import io.kazuki.v0.store.schema.model.Attribute;
+import io.kazuki.v0.store.schema.model.IndexDefinition;
+import io.kazuki.v0.store.schema.model.Schema;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import org.joda.time.DateTime;
+
+import com.google.common.collect.ImmutableList;
+
+public class Everything {
+  public enum TestEnum {
+    ONE, TWO, THREE, FOUR
+  };
+
+  public static final Schema EVERYTHING_SCHEMA;
+  static {
+    List<Attribute> attrs = new ArrayList<Attribute>();
+
+    attrs.add(new Attribute("theAny", Attribute.Type.ANY, null, true));
+    attrs.add(new Attribute("theMap", Attribute.Type.MAP, null, true));
+    attrs.add(new Attribute("theArray", Attribute.Type.ARRAY, null, true));
+    attrs.add(new Attribute("theBoolean", Attribute.Type.BOOLEAN, null, true));
+    attrs.add(new Attribute("theCharOne", Attribute.Type.CHAR_ONE, null, true));
+
+    //
+    // BEWARE: enums aren't auto linked to Java enums because it's important
+    // that schema enum values are never removed!
+    //
+    attrs.add(new Attribute("theEnum", Attribute.Type.ENUM, ImmutableList.<Object>of("ZERO", "ONE",
+        "TWO", "THREE", "FOUR"), true));
+
+    attrs.add(new Attribute("theU8", Attribute.Type.U8, null, true));
+    attrs.add(new Attribute("theU16", Attribute.Type.U16, null, true));
+    attrs.add(new Attribute("theU32", Attribute.Type.U32, null, true));
+    attrs.add(new Attribute("theU64", Attribute.Type.U64, null, true));
+
+    attrs.add(new Attribute("theI8", Attribute.Type.I8, null, true));
+    attrs.add(new Attribute("theI16", Attribute.Type.I16, null, true));
+    attrs.add(new Attribute("theI32", Attribute.Type.I32, null, true));
+    attrs.add(new Attribute("theI64", Attribute.Type.I64, null, true));
+
+    attrs.add(new Attribute("theUtcDate", Attribute.Type.UTC_DATE_SECS, null, true));
+    attrs.add(new Attribute("theUtf8SmallString", Attribute.Type.UTF8_SMALLSTRING, null, true));
+    attrs.add(new Attribute("theUtf8Text", Attribute.Type.UTF8_SMALLSTRING, null, true));
+
+    List<IndexDefinition> indexDefs = new ArrayList<IndexDefinition>();
+
+    // indexDefs.add(new IndexDefinition("uniqueFooKeyValue", ImmutableList.of(new IndexAttribute(
+    // "fooKey", SortDirection.ASCENDING, AttributeTransform.NONE), new IndexAttribute("fooValue",
+    // SortDirection.ASCENDING, AttributeTransform.NONE)), true));
+
+    // indexDefs.add(new IndexDefinition("fooKey", ImmutableList.of(new IndexAttribute("fooKey",
+    // SortDirection.ASCENDING, AttributeTransform.NONE)), false));
+
+    EVERYTHING_SCHEMA =
+        new Schema(Collections.unmodifiableList(attrs), Collections.unmodifiableList(indexDefs));
+  }
+
+  public Object theAny;
+  public Map theMap;
+  public List theArray;
+  public Boolean theBoolean;
+  public Character theCharOne;
+  public TestEnum theEnum;
+  public Long theU8;
+  public Long theU16;
+  public Long theU32;
+  public BigInteger theU64;
+  public Long theI8;
+  public Long theI16;
+  public Long theI32;
+  public Long theI64;
+  public DateTime theUtcDate;
+  public String theUtf8SmallString;
+  public String theUtf8Text;
+}
---
+++ kazuki-impl/src/test/java/io/kazuki/v0/store/keyvalue/KeyValueStoreAttributeTypesTest.java
@@ -0,0 +1,132 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package io.kazuki.v0.store.keyvalue;
+
+import io.kazuki.v0.internal.helper.Configurations;
+import io.kazuki.v0.internal.helper.EncodingHelper;
+import io.kazuki.v0.store.Everything;
+import io.kazuki.v0.store.KazukiException;
+import io.kazuki.v0.store.easy.EasyKeyValueStoreModule;
+import io.kazuki.v0.store.lifecycle.Lifecycle;
+import io.kazuki.v0.store.lifecycle.LifecycleModule;
+import io.kazuki.v0.store.schema.SchemaStore;
+import io.kazuki.v0.store.schema.TypeValidation;
+
+import java.math.BigInteger;
+
+import org.joda.time.DateTime;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Key;
+import com.google.inject.name.Names;
+
+/**
+ * Attribute Types smoke test.
+ */
+@Test
+public class KeyValueStoreAttributeTypesTest {
+  protected Lifecycle lifecycle;
+  protected SchemaStore schema;
+  protected KeyValueStore kvStore;
+
+  @BeforeMethod
+  public void prepare() throws Exception {
+    Injector injector =
+        Guice.createInjector(
+            new LifecycleModule(ExampleStore.STORE_NAME),
+            new EasyKeyValueStoreModule(ExampleStore.STORE_NAME, null)
+                .withJdbiConfig(Configurations.getJdbi().build())
+                .withKeyValueStoreConfig(
+                    Configurations.getKeyValue(ExampleStore.GROUP_NAME, ExampleStore.STORE_NAME)
+                        .build())
+                .withSequenceConfig(
+                    Configurations.getSequence(ExampleStore.GROUP_NAME, ExampleStore.STORE_NAME)
+                        .build()));
+
+    lifecycle =
+        injector.getBinding(Key.get(Lifecycle.class, Names.named(ExampleStore.STORE_NAME)))
+            .getProvider().get();
+
+    schema =
+        injector.getBinding(Key.get(SchemaStore.class, Names.named(ExampleStore.STORE_NAME)))
+            .getProvider().get();
+
+    kvStore =
+        injector.getBinding(Key.get(KeyValueStore.class, Names.named(ExampleStore.STORE_NAME)))
+            .getProvider().get();
+
+    lifecycle.init();
+    lifecycle.start();
+  }
+
+  @AfterMethod
+  public void cleanup() {
+    lifecycle.stop();
+    lifecycle.shutdown();
+  }
+
+  public static class ExampleStore {
+    public static final String GROUP_NAME = "example";
+    public static final String STORE_NAME = "example";
+  }
+
+  @Test
+  public void emptySchema() throws Exception {
+    try {
+      schema.createSchema("everything", Everything.EVERYTHING_SCHEMA);
+    } catch (KazukiException e) {
+      e.printStackTrace();
+    }
+
+    Everything e1 = new Everything();
+
+    e1.theAny =
+        new BigInteger(
+            "111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111");
+    e1.theMap = ImmutableMap.of("foo", 1L, "bar", true);
+    e1.theArray = ImmutableList.of("a", false, 6L);
+    e1.theUtcDate = new DateTime();
+    e1.theCharOne = 'C';
+    e1.theEnum = Everything.TestEnum.FOUR;
+
+    e1.theI8 = Long.valueOf((1 << 7) - 1);
+    e1.theI16 = Long.valueOf((1 << 15) - 1);
+    e1.theI32 = Long.valueOf((1L << 31) - 1L);
+    e1.theI64 = Long.valueOf((1L << 63) - 1L);
+
+    e1.theU8 = Long.valueOf((1 << 8) - 1);
+    e1.theU16 = Long.valueOf((1 << 16) - 1);
+    e1.theU32 = Long.valueOf((1L << 32) - 1L);
+    e1.theU64 = new BigInteger("1").shiftLeft(64).subtract(new BigInteger("1"));
+
+    e1.theUtf8SmallString =
+        "okokokokokokokokokokokokokokokokokokokokokokokokokokokokokokokokokokokokokokokok";
+    e1.theUtf8Text = "this would be a big string";
+
+    io.kazuki.v0.store.Key k1 =
+        kvStore.create("everything", Everything.class, e1, TypeValidation.STRICT);
+
+    Everything v1 = kvStore.retrieve(k1, Everything.class);
+
+    Assert.assertEquals(EncodingHelper.convertToJson(v1), EncodingHelper.convertToJson(e1));
+  }
+}
