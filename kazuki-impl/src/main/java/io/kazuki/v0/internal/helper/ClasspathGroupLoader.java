afda07bf99ee555d738df05cc529d77b8e95f972
Sunny Gleason <sunny.gleason@gmail.com>
2014-06-16 09:36:19 -0400

    update license headers (thanks @mcculls)

--- kazuki-impl/src/main/java/io/kazuki/v0/internal/helper/ClasspathGroupLoader.java
+++ kazuki-impl/src/main/java/io/kazuki/v0/internal/helper/ClasspathGroupLoader.java
@@ -1,4 +1,5 @@
-/* Copyright 2004-2007 Brian McCallister
+/**
+ * Copyright 2014 Sunny Gleason and original author or authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package io.kazuki.v0.internal.helper;
 
 import antlr.CharScanner;
